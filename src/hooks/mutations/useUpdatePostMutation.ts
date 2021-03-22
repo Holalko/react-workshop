77851feb8f4b237538f20fb687efb9d10d041585
Jakub <hol0373@vsb.cz>
2021-03-22 17:30:10 +0100

    Completed task #4

--- src/hooks/mutations/useUpdatePostMutation.ts
+++ src/hooks/mutations/useUpdatePostMutation.ts
@@ -1,7 +1,12 @@
 import {useMutation} from "react-query";
 import {PostType} from "../../models/PostType";
 import {putRequest, request} from "../../helpers/request";
+import {invalidatePosts} from "../queries/usePosts";
 
 export const useCreatePostMutation = () => {
-    return useMutation((post: PostType) => putRequest(`posts/${post.id}`, post));
+    return useMutation((post: PostType) => putRequest(`posts/${post.id}`, post), {
+        onSuccess: () => {
+            invalidatePosts();
+        }
+    });
 }
